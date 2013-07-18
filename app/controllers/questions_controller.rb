2fb2a3456c518173741a12eefd1eaa8f696660fa
Apprentice <apprentice@devbootcamp.com>
2013-07-17 17:55:49 -0700

    Added form and  new and create functions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,17 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
+
   def show
     @question = Question.find(params[:id])
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.create(params[:question])
+    redirect_to question_path(@question)
   end
 end
