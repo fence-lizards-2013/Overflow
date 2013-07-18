c7bcca39360befe4ed5527f51f7c9b3d8bea95c1
Apprentice <apprentice@devbootcamp.com>
2013-07-17 19:13:27 -0700

    added question index view tests, passed

---
+++ spec/views/questions/new.html.erb_spec.rb
@@ -0,0 +1,22 @@
+# require "spec_helper"
+
+# describe "questions/new.html.erb" do
+#   before(:each) do
+#     assign(:questions, [
+#         stub_model(Question, :title => "Test title", :content => "Testoignwo ing"),
+#         stub_model(Question, :title => "Test title 2", :content => "Twenweo siengmose im"),
+#         ])
+#     render
+#   end
+
+#   it "should have awesome header" do
+#     visit new_question_path-rails
+#     expect(render).to have_content("DO I LOOK FAT IN DIS??")
+#   end
+
+#   it "should have subheader of form" do
+#     visit '/questions/new.html.erb'
+#     expect(render).to have_content("New Question Form")
+#   end
+# end
+
