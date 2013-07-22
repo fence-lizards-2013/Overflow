ff9e0aabe4f86c44fc5d9fbcd021c55a89211207
Apprentice <apprentice@devbootcamp.com>
2013-07-21 20:27:58 -0700

    added tests for edit page

--- spec/features/users_show_spec.rb
+++ spec/features/users_show_spec.rb
@@ -4,6 +4,8 @@ describe "User Show Page" do
 	let(:name) { "mca" }
 	let(:email) {"test@test.com"}
 	let(:password) {"12345678"}
+	let(:updated_email) { "updated_test@test.com" }
+	let(:updated_password) { "password" }
 
 	before(:each) do
 		@user = User.create(name: name, email: email, password: password)
@@ -28,5 +30,33 @@ describe "User Show Page" do
 			click_link('Edit Account')
 			expect(page).to have_content("Edit User")
 		end
+
+		it "should allow you to update the email for the user" do 
+			visit user_path(@user)
+			click_link('Edit Account')
+			fill_in 'Email', with: updated_email
+			fill_in 'user_current_password', with: password
+			click_button('Update')
+			expect(User.find_by_email(updated_email)).to_not be_nil
+		end
+
+		it "should allow you to update the password for the user" do 
+			visit user_path(@user)
+			click_link('Edit Account')
+			fill_in 'Email', with: email
+			fill_in 'user_password', with: updated_password
+			fill_in 'user_password_confirmation', with: updated_password
+			fill_in 'user_current_password', with: password
+			click_button('Update')
+			
+			click_link('Logout')
+			click_link('Sign In')
+			fill_in 'Email', with: email
+			fill_in 'Password', with: updated_password
+			click_button "Sign in"
+			expect(page).to have_content('New Question')
+		end
+
+		
 	end
 end
